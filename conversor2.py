c0c7fa0201a6570b747b3b6a2f5a53742e7a22fe
Gabriela Cruz Molina <gabcruzm1@gmail.com>
2021-09-29 21:59:39 -0400

    Delete conversor2.py

--- conversor2.py
+++
@@ -1,7 +0,0 @@
-pesos = input("¿Cuántos pesos mexicanos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 20.52
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dólares")
