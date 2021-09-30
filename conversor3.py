c8663b1910cf41be860438dbb0a372ec8036be46
Gabriela Cruz Molina <gabcruzm1@gmail.com>
2021-09-29 21:59:46 -0400

    Delete conversor3.py

--- conversor3.py
+++
@@ -1,7 +0,0 @@
-dolares = input("¿Cuántos dólares tienes?: ")
-dolares = float(dolares)
-valor_peso = 0.0003
-pesos = dolares / valor_peso
-pesos = round(pesos, 2)
-pesos = str(pesos)
-print("Tienes $" + pesos + " pesos colombianos")
